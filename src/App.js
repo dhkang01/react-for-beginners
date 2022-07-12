8b36ad3499969e2a0da8a46753a21286e798dc23
DongHoon <dhkang01@gmail.com>
2022-07-12 18:22:42 +0900

    start

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import styles from "./App.module.css"
+import {useState, useEffect} from "react"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom"
+import Home from "./routes/Home.js"
+import Detail from "./routes/Detail.js"
+
+
+function App() {
+  return <Router>
+    <Routes>
+      <Route path="/movie/:id" element={<Detail />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </Router>
+}
+
+export default App;
